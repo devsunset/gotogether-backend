9ac988668aed515bdd101dd4627cc6740c86062e
devsunset <devsunset@gmail.com>
2022-07-15 16:23:41 +0900

    dev

--- src/main/java/com/gotogether/service/PostService.java
+++ src/main/java/com/gotogether/service/PostService.java
@@ -1,13 +1,18 @@
 package com.gotogether.service;
 
 import com.gotogether.entity.Post;
+import com.gotogether.entity.User;
 import com.gotogether.payload.request.PostCreateRequest;
 import com.gotogether.repository.PostRepository;
 import com.gotogether.repository.UserRepository;
+import com.gotogether.system.security.service.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
 
@@ -26,10 +31,14 @@ public class PostService {
     @Autowired
     private final ModelMapper modelMapper;
 
-    public ResponseEntity<?> save(PostCreateRequest postCreateRequest) {
+    public Post save(PostCreateRequest postCreateRequest) {
         log.debug("#################postCreateRequest###############"+postCreateRequest.toString());
         Post post = modelMapper.map(postCreateRequest, Post.class);
+        UserDetails userDetail = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByUsername(userDetail.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + userDetail.getUsername()));
+        post.setWriter(user);
         log.debug("#################post###############"+post.toString());
-        return null;
+        return postRepository.save(post);
     }
 }
