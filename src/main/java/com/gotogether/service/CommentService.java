03ca4452e8bd05ae2bb129fe76f8733675669e35
devsunset <devsunset@gmail.com>
2022-07-18 16:18:48 +0900

    dev

--- src/main/java/com/gotogether/service/CommentService.java
+++ src/main/java/com/gotogether/service/CommentService.java
@@ -2,14 +2,14 @@ package com.gotogether.service;
 
 import com.gotogether.dto.request.CommentRequest;
 import com.gotogether.entity.Comment;
-import com.gotogether.entity.Post;
-import com.gotogether.entity.User;
 import com.gotogether.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService{
@@ -23,15 +23,14 @@ public class CommentService{
 
     public Long save(CommentRequest commentRequest) throws Exception {
         Comment comment = modelMapper.map(commentRequest, Comment.class);
+        log.debug("################## : "+comment.toString());
         comment.setWriter(authService.getSessionUser());
         return commentRepository.save(comment).getComment_id();
     }
 
     public Long update(Long comment_id, CommentRequest commentRequest) throws Exception {
         Comment comment = modelMapper.map(commentRequest, Comment.class);
-        User user = authService.getSessionUser();
-        //user.getRoles()
-        comment.setWriter(user);
+        comment.setWriter(authService.getSessionUser());
         return commentRepository.save(comment).getComment_id();
     }
 
