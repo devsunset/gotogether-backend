4608f4fdbd0efc9c74b16a9f77f7cac91ebb26f5
devsunset <devsunset@gmail.com>
2022-08-06 19:55:43 +0900

    dev

--- src/main/java/com/gotogether/system/enums/SkillLevelType.java
+++ src/main/java/com/gotogether/system/enums/SkillLevelType.java
@@ -1,12 +1,11 @@
 package com.gotogether.system.enums;
 
 public enum SkillLevelType {
-    INTEREST("INTEREST"),
-    HELLO_WORLD("HELLO_WORLD"),
+
     BASIC("BASIC"),
-    TOY_PROJECT("TOY_PROJECT"),
-    JOB_BEGINNER("JOB_BEGINNER"),
-    JOB_PROFESSIONAL("JOB_PROFESSIONAL");
+    JOB("JOB"),
+    INTEREST("INTEREST"),
+    TOY_PROJECT("TOY_PROJECT");
 
     final private String name;
 
