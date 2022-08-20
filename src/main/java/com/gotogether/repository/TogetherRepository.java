cda201ae706d78b16027139040b1c22c18494b91
devsunset <devsunset@gmail.com>
2022-08-20 11:23:33 +0900

    dev

--- src/main/java/com/gotogether/repository/TogetherRepository.java
+++ src/main/java/com/gotogether/repository/TogetherRepository.java
@@ -19,7 +19,7 @@ public interface TogetherRepository extends JpaRepository<Together, Long> {
 
     List<Together> findTop3ByTogetherIdGreaterThanOrderByTogetherIdDesc(Long togetherId);
 
-    Page<Together> findByTitleLikeIgnoreCaseOrContentLikeIgnoreCaseOrPurposeLikeIgnoreCaseOrCategoryLikeIgnoreCaseOrInvolveTypeLikeIgnoreCaseOrSkillLikeIgnoreCase(String title, String content, String purpose, String category, String involveType, String skill, Pageable pageable);
+    Page<Together> findByTitleContainsIgnoreCaseOrContentContainsIgnoreCaseOrPurposeContainsIgnoreCaseOrCategoryContainsIgnoreCaseOrInvolveTypeContainsIgnoreCaseOrSkillContainsIgnoreCase(String title, String content, String purpose, String category, String involveType, String skill, Pageable pageable);
 
 }
 
