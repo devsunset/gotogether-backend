e794427cd762fd83c223c845f38b158a5454093a
devsunset <devsunset@gmail.com>
2022-08-24 14:43:41 +0900

    dev

--- src/main/java/com/gotogether/system/configuration/WebSecurityConfig.java
+++ src/main/java/com/gotogether/system/configuration/WebSecurityConfig.java
@@ -57,7 +57,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/v3/api-docs", "/swagger*/**").permitAll()
